55579f2edb8ba47c3576d3a9b1752250cc38aec2
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-02-18 02:54:25 -0300

    Exercicio polimorfismo

---
+++ cursoNelioAlves/src/main/java/exercicioPolimorfismo/application/Program.java
@@ -0,0 +1,59 @@
+package exercicioPolimorfismo.application;
+
+import exercicioPolimorfismo.entities.ImportedProduct;
+import exercicioPolimorfismo.entities.Product;
+import exercicioPolimorfismo.entities.UsedProduct;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            System.out.println("Product #" + i + " data:");
+            System.out.print("Common, used or imported (c/u/i)? ");
+            char type = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+            if (type == 'c') {
+                list.add(new Product(name, price));
+            }
+            else if (type == 'u') {
+                System.out.print("Manufacture date (DD/MM/YYYY): ");
+                LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                list.add(new UsedProduct(name, price, date));
+            }
+            else {
+                System.out.print("Custom free: ");
+                double customFree = sc.nextDouble();
+                list.add(new ImportedProduct(name, price, customFree));
+            }
+        }
+
+        System.out.println();
+        System.out.println("PRICE TAGS:");
+        for (Product prod : list) {
+            System.out.println(prod.priceTag());
+        }
+
+        sc.close();
+    }
+}
---
+++ cursoNelioAlves/src/main/java/exercicioPolimorfismo/entities/ImportedProduct.java
@@ -0,0 +1,33 @@
+package exercicioPolimorfismo.entities;
+
+public class ImportedProduct extends Product {
+
+    private Double customFree;
+
+    public ImportedProduct(String name, Double price, Double customFree) {
+        super(name, price);
+        this.customFree = customFree;
+    }
+
+    public Double getCustomFree() {
+        return customFree;
+    }
+
+    public void setCustomFree(Double customFree) {
+        this.customFree = customFree;
+    }
+
+    public Double totalPrice() {
+        return getPrice() + customFree;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName()
+                + " $ "
+                + String.format("%.2f", totalPrice())
+                + " (Custom free: $ "
+                + String.format("%.2f", customFree)
+                + ")";
+    }
+}
